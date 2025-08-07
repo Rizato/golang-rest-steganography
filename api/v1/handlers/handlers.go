ba5ef1bc545a17c4aebee6ff4b9893ee74a285c2
Robert Lathrop <rizatodev@gmail.com>
2025-08-07 15:04:51 -0400

    Add Decode implementation
    Improve json writing with a func

--- api/v1/handlers/handlers.go
+++ api/v1/handlers/handlers.go
@@ -9,6 +9,12 @@ import (
 	files "steg/files"
 )
 
+func writeJSON(w http.ResponseWriter, status int, data interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(data)
+}
+
 type EncodeHandler struct {
 	jobs      map[uuid.UUID]*models.EncodeJob
 	validator files.FileValidator
@@ -28,12 +34,18 @@ func (h *EncodeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (h *EncodeHandler) CreateJob() *models.EncodeJob {
+	job := models.NewEncodeJob()
+	// Store job in state
+	h.jobs[job.Uuid] = job
+	return job
+}
+
+// POST /api/v1/encode
 // Create a new job for steg, read multi-part file upload
 func (h *EncodeHandler) EncodePostHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a new job
-	job := models.NewEncodeJob()
-	// Add job to state
-	h.jobs[job.Uuid] = job
+	job := h.CreateJob()
 
 	// Read file from multipart form, and save it
 	saver := files.NewMultiPartFileSaver(r, "image", "raw", job.Uuid.String(), h.validator)
@@ -43,15 +55,18 @@ func (h *EncodeHandler) EncodePostHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 	job.RawImagePath = name
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusAccepted)
+	err = writeJSON(w, http.StatusAccepted, job)
+	if err != nil {
+		http.Error(w, "400 Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 	// Starts a process in goroutine
 	job.ProcessImage()
 }
 
-// Get steg state/image
+// GET /api/v1/encode/{id}
 func (h *EncodeHandler) EncodeGetHandler(w http.ResponseWriter, r *http.Request) {
-	uuidString := r.URL.Query().Get("uuid")
+	uuidString := r.PathValue("id")
 	u, err := uuid.Parse(uuidString)
 	if err != nil {
 		http.Error(w, "400 Bad Request", http.StatusBadRequest)
@@ -62,14 +77,12 @@ func (h *EncodeHandler) EncodeGetHandler(w http.ResponseWriter, r *http.Request)
 		http.Error(w, "404 Not Found", http.StatusNotFound)
 		return
 	}
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	t, err := json.MarshalIndent(job, "", "  ")
+
+	err = writeJSON(w, http.StatusAccepted, job)
 	if err != nil {
-		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+		http.Error(w, "400 Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	w.Write(t)
 }
 
 type DecodeHandler struct {
@@ -79,6 +92,13 @@ type DecodeHandler struct {
 
 func NewDecodeHandler(validator files.FileValidator) *DecodeHandler {
 	return &DecodeHandler{jobs: make(map[uuid.UUID]*models.DecodeJob), validator: validator}
+}
+
+func (h *DecodeHandler) CreateJob() *models.DecodeJob {
+	job := models.NewDecodeJob()
+	// Store job in state
+	h.jobs[job.Uuid] = job
+	return job
 }
 
 func (h *DecodeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -91,7 +111,7 @@ func (h *DecodeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Create a new job to decode
+// POST /api/v1/decode
 func (h *DecodeHandler) DecodePostHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a new job
 	job := models.NewDecodeJob()
@@ -106,18 +126,38 @@ func (h *DecodeHandler) DecodePostHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 	job.ImagePath = name
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusAccepted)
+	err = writeJSON(w, http.StatusAccepted, job)
+	if err != nil {
+		http.Error(w, "400 Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 	// Starts a process in goroutine
 	job.ProcessImage()
 }
 
-// Get decode job/message
+// GET /api/v1/decode/{id}
 func (h *DecodeHandler) DecodeGetHandler(w http.ResponseWriter, r *http.Request) {
+	uuidString := r.PathValue("id")
+	u, err := uuid.Parse(uuidString)
+	if err != nil {
+		http.Error(w, "400 Bad Request", http.StatusBadRequest)
+		return
+	}
+	job := h.jobs[u]
+	if job == nil {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
 
+	err = writeJSON(w, http.StatusAccepted, job)
+	if err != nil {
+		http.Error(w, "400 Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 // Get some generic stats
+// GET /api/v1/stats
 func GetStats(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello, Stats")
 }
